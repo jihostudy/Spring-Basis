4661a47c400935f91db6f9f465c9d19d9a7eac59
김지호 Jiho Kim <cale.jiho@gmail.com>
2025-12-05 14:38:40 +0900

    AppConfig에 구현체와 역할이 분명하도록 리펙토링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.repository.MemberRepository;
 import hello.core.member.repository.MemoryMemberRepository;
 import hello.core.member.service.MemberService;
 import hello.core.member.service.MemberServiceImpl;
@@ -11,10 +13,18 @@ public class AppConfig {
 
   // AppConfig에서 구현체를 생성하여 제공, 각 서비스는 인터페이스에만 의존하고 구현체에는 의존하지 않음
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+
+  public DiscountPolicy discountPolicy() {
+    return new FixDiscountPolicy();
   }
 }
